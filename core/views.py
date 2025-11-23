cde213e6eda09147f94a63eab4aaf510674e2632
MHanzlaJamil <muhammadhanzalaj@gmail.com>
2025-11-23 23:56:48 +0500

    dashboard fix

--- core/views.py
+++ core/views.py
@@ -470,7 +470,8 @@ def dashboard_view(request):
             "employees_per_department": dept_emp_list,
             "pending_leaves": pending_leaves,
             "total_payroll": float(total_payroll),
-            "increase_in_hires_percentage": float(new_hires / (new_hires + prev_hires)) * 100
+            "increase_in_hires_percentage": float(new_hires / (new_hires + prev_hires)) * 100,
+            "new_hires" : new_hires
         }, status=200)
     except Exception as e:
         return JsonResponse ({'message': f"{e}"})
